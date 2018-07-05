f76982bae897761350162cd5480fce66402c9db3
Daniel Leitner <dmcleite@hotmail.com>
2018-07-05 10:14:34 +0200

    implemented uniform sampling with reducing redundand frames.

--- VideoRetrieval/videoretrieval/src/main/java/videoretrieval/VideoAnalyzer.java
+++ VideoRetrieval/videoretrieval/src/main/java/videoretrieval/VideoAnalyzer.java
@@ -1,46 +1,79 @@
 package videoretrieval;
 
+
 import java.util.ArrayList;
+import java.util.List;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.videoio.Videoio;
+import org.opencv.imgproc.Imgproc;
 public class VideoAnalyzer {
 	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
-	private String videoPath;
 	private VideoCapture video;
-	public VideoAnalyzer(String videoPath) {
-		super();
-		this.videoPath = videoPath;
+	public VideoAnalyzer() {
+	}
+	
+	/**
+	 * Extracts frames from given video.
+	 * @param videoPath path to video file.
+	 * @param fps Determines how many Frames per second are inspected.
+	 * @param th Threshold for Histogram comparison between inspeced frames (correlation metric). [-1;1]
+	 * @return Extracted Frames.
+	 */
+	public ArrayList<Mat> extractKeyFrames(String videoPath, int fps, double th) {
 		video = new VideoCapture(videoPath);
 		System.out.println("FPS: " + video.get(Videoio.CAP_PROP_FPS));
 		System.out.println("Frames: " + video.get(Videoio.CAP_PROP_FRAME_COUNT));
-	}
-
-	public String getVideoPath() {
-		return videoPath;
-	}
-
-	public void setVideoPath(String videoPath) {
-		this.videoPath = videoPath;
-	}
-	
-	public ArrayList<Mat> extractKeyFrames() {
+		
 		Mat frame = new Mat();
-		int distance = 60;
+		Mat hsvHist = new Mat();
+		boolean isFirstFrame = true;
+		int distance = (int) Math.round(video.get(Videoio.CAP_PROP_FPS))/fps;
 		int numOfFrames = 0;
+		
+	    int hbins = 30, sbins = 32;
+	    MatOfInt histSize = new MatOfInt(hbins, sbins);
+	    MatOfFloat histRange = new MatOfFloat(0f, 180f, 0f, 256f);
+	    List<Mat> imageList = new ArrayList<Mat>();
+	    
 		ArrayList<Mat> frames = new ArrayList<Mat>();
 	    while(video.read(frame)) { 
 	    	if(!frame.empty())
 	    	{
 	    		numOfFrames++;
-	    		if(numOfFrames%60 == 1)
-	    			frames.add(frame);
+	    		if(numOfFrames%distance == 1) {
+	    			if(isFirstFrame) {
+	    				isFirstFrame = false;
+	    				frames.add(frame.clone());
+	    				Mat hsvframe = new Mat();
+	    				Imgproc.cvtColor(frame, hsvframe, Imgproc.COLOR_BGR2HSV);
+	    				imageList = new ArrayList<Mat>();
+	    			    imageList.add(hsvframe);
+	    			    Imgproc.calcHist(imageList, new MatOfInt(0, 1), new Mat(), hsvHist, histSize, histRange);
+	    			} else {
+	    				Mat newhsvframe = new Mat();
+	    				Imgproc.cvtColor(frame, newhsvframe, Imgproc.COLOR_BGR2HSV);
+	    				
+	    			    imageList = new ArrayList<Mat>();
+	    			    imageList.add(newhsvframe);
+	    			    Mat newhsvHist = new Mat();
+	    			    Imgproc.calcHist(imageList, new MatOfInt(0, 1), new Mat(), newhsvHist, histSize, histRange);
+	    			    
+	    			    double dist = Imgproc.compareHist(hsvHist, newhsvHist, Imgproc.CV_COMP_CORREL);
+	    				if(dist < th) {
+		    			    System.out.println("distance (" + frames.size() + "," + (frames.size() + 1) + "): " + dist);
+	    					frames.add(frame.clone());
+	    					hsvHist = newhsvHist;
+	    				}
+	    			}
+	    		}
 	    	}
 	        	
 	   }
 	   return frames;
-	}
-	
+	}	
 }
--- VideoRetrieval/videoretrieval/src/main/java/videoretrieval/main.java
+++ VideoRetrieval/videoretrieval/src/main/java/videoretrieval/main.java
@@ -15,29 +15,18 @@ import org.opencv.imgcodecs.Imgcodecs;
 public class main {
 	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 	private static final String path = "C:/Videos";
-	private static final String testvideo = path + "/35771.mp4";
+	private static final String testvideo = path + "/35368.mp4";
 	public static void main(String[] args) throws Exception {
 		System.out.println(testvideo);
-		VideoAnalyzer va = new VideoAnalyzer(testvideo);
-		ArrayList<Mat> frames = va.extractKeyFrames();
+		VideoAnalyzer va = new VideoAnalyzer();
+		ArrayList<Mat> frames = va.extractKeyFrames(testvideo, 1, 0.4);
+		saveFrames(frames);
+	}
+	
+	private static void saveFrames(ArrayList<Mat> frames) {
 		String imgPath = path + "/Imgs/";
 		for(int i = 0; i<frames.size(); i++) {
-			/*BufferedImage bi = Mat2BufferedImage(frames.get(i));
-			File outputfile = new File(imgPath + (i+1) + ".jpg");
-			ImageIO.write(bi, "jpg", outputfile);*/
-
-
 			Imgcodecs.imwrite(imgPath + (i+1) + ".jpg", frames.get(i));
 		}
 	}
-	
-	/*static BufferedImage Mat2BufferedImage(Mat matrix)throws Exception {        
-	    MatOfByte mob=new MatOfByte();
-	    Imgcodecs.imencode(".jpg", matrix, mob);
-	    byte ba[]=mob.toArray();
-
-	    BufferedImage bi=ImageIO.read(new ByteArrayInputStream(ba));
-	    return bi;
-	}*/
-
 }
