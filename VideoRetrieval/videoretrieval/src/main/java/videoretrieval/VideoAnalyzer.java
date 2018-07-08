0ab12c9343080e14b8d4bd81c2c862131ab301a9
Jameson Steiner <jameson.steiner@gmail.com>
2018-07-08 09:24:34 +0200

    Pull histogram calculation into own method
    Additionally, restructure code a bit.

--- VideoRetrieval/videoretrieval/src/main/java/videoretrieval/VideoAnalyzer.java
+++ VideoRetrieval/videoretrieval/src/main/java/videoretrieval/VideoAnalyzer.java
@@ -11,69 +11,77 @@ import org.opencv.core.MatOfInt;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.videoio.Videoio;
 import org.opencv.imgproc.Imgproc;
+
 public class VideoAnalyzer {
-	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
-	private VideoCapture video;
-	public VideoAnalyzer() {
-	}
-	
-	/**
-	 * Extracts frames from given video.
-	 * @param videoPath path to video file.
-	 * @param fps Determines how many Frames per second are inspected.
-	 * @param th Threshold for Histogram comparison between inspeced frames (correlation metric). [-1;1]
-	 * @return Extracted Frames.
-	 */
-	public ArrayList<Mat> extractKeyFrames(String videoPath, int fps, double th) {
-		video = new VideoCapture(videoPath);
-		System.out.println("FPS: " + video.get(Videoio.CAP_PROP_FPS));
-		System.out.println("Frames: " + video.get(Videoio.CAP_PROP_FRAME_COUNT));
-		
-		Mat frame = new Mat();
-		Mat hsvHist = new Mat();
-		boolean isFirstFrame = true;
-		int distance = (int) Math.round(video.get(Videoio.CAP_PROP_FPS))/fps;
-		int numOfFrames = 0;
-		
-	    int hbins = 30, sbins = 32;
-	    MatOfInt histSize = new MatOfInt(hbins, sbins);
-	    MatOfFloat histRange = new MatOfFloat(0f, 180f, 0f, 256f);
-	    List<Mat> imageList = new ArrayList<Mat>();
-	    
-		ArrayList<Mat> frames = new ArrayList<Mat>();
-	    while(video.read(frame)) { 
-	    	if(!frame.empty())
-	    	{
-	    		numOfFrames++;
-	    		if(numOfFrames%distance == 1) {
-	    			if(isFirstFrame) {
-	    				isFirstFrame = false;
-	    				frames.add(frame.clone());
-	    				Mat hsvframe = new Mat();
-	    				Imgproc.cvtColor(frame, hsvframe, Imgproc.COLOR_BGR2HSV);
-	    				imageList = new ArrayList<Mat>();
-	    			    imageList.add(hsvframe);
-	    			    Imgproc.calcHist(imageList, new MatOfInt(0, 1), new Mat(), hsvHist, histSize, histRange);
-	    			} else {
-	    				Mat newhsvframe = new Mat();
-	    				Imgproc.cvtColor(frame, newhsvframe, Imgproc.COLOR_BGR2HSV);
-	    				
-	    			    imageList = new ArrayList<Mat>();
-	    			    imageList.add(newhsvframe);
-	    			    Mat newhsvHist = new Mat();
-	    			    Imgproc.calcHist(imageList, new MatOfInt(0, 1), new Mat(), newhsvHist, histSize, histRange);
-	    			    
-	    			    double dist = Imgproc.compareHist(hsvHist, newhsvHist, Imgproc.CV_COMP_CORREL);
-	    				if(dist < th) {
-		    			    System.out.println("distance (" + frames.size() + "," + (frames.size() + 1) + "): " + dist);
-	    					frames.add(frame.clone());
-	    					hsvHist = newhsvHist;
-	    				}
-	    			}
-	    		}
-	    	}
-	        	
-	   }
-	   return frames;
-	}	
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    public VideoAnalyzer() {
+    }
+
+    private Mat calcHist(Mat frame) {
+        Mat hsvHist = new Mat();
+        int hbins = 30, sbins = 32;
+        MatOfInt histSize = new MatOfInt(hbins, sbins);
+        MatOfFloat histRange = new MatOfFloat(0f, 180f, 0f, 256f);
+
+        Mat hsvframe = new Mat();
+        Imgproc.cvtColor(frame, hsvframe, Imgproc.COLOR_BGR2HSV);
+        List<Mat> imageList = new ArrayList<Mat>();
+        imageList = new ArrayList<Mat>();
+        imageList.add(hsvframe);
+        Imgproc.calcHist(imageList, new MatOfInt(0, 1), new Mat(), hsvHist, histSize, histRange);
+
+        return hsvHist;
+    }
+
+    /**
+     * Extracts frames from given video.
+     *
+     * @param videoPath path to video file.
+     * @param fps       Determines how many Frames per second are inspected.
+     * @param th        Threshold for Histogram comparison between inspected frames (correlation metric). [-1;1]
+     * @return Extracted Frames.
+     */
+    public ArrayList<Mat> extractKeyFrames(String videoPath, int fps, double th) {
+        VideoCapture video = new VideoCapture(videoPath);
+
+        System.out.println("FPS: " + video.get(Videoio.CAP_PROP_FPS));
+        System.out.println("Frames: " + video.get(Videoio.CAP_PROP_FRAME_COUNT));
+
+        Mat frame = new Mat();
+        ArrayList<Mat> frames = new ArrayList<Mat>();
+        Mat previousFrameHist = new Mat();
+        Mat nextFrameHist = new Mat();
+
+        int samplingDistance = (int) Math.round(video.get(Videoio.CAP_PROP_FPS)) / fps;
+        boolean isFirstFrame = true;
+        int frameCount = 0;
+
+        while (video.read(frame)) {
+            if (frame.empty()) { continue; }
+            frameCount++;
+
+            if (frameCount % samplingDistance == 1) {
+                if (isFirstFrame) {
+                    isFirstFrame = false;
+                    frames.add(frame.clone());
+                    previousFrameHist = this.calcHist(frame);
+                } else {
+                    nextFrameHist = this.calcHist(frame);
+                    double distance = Imgproc.compareHist(previousFrameHist, nextFrameHist, Imgproc.CV_COMP_CORREL);
+
+                    if (distance < th) {
+                        System.out.println("distance (" + frames.size() + "," + (frames.size() + 1) + "): " + distance);
+                        frames.add(frame.clone());
+
+                        // TODO this must be moved outside the if?
+                        previousFrameHist = nextFrameHist;
+                    }
+                }
+            }
+        }
+        return frames;
+    }
 }
