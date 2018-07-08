b12ecbd6ad0ad2cd96a089a05609caa07ed27a3c
Wolfram Hofmeister <wolfram.hofmeister@gmail.com>
2018-07-08 14:34:54 +0200

    Added primitive main menu

--- VideoRetrieval/videoretrieval/src/main/java/videoretrieval/main.java
+++ VideoRetrieval/videoretrieval/src/main/java/videoretrieval/main.java
@@ -1,6 +1,7 @@
 package videoretrieval;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -19,20 +20,70 @@ public class main {
 
 	public static void main(String[] args) throws Exception {
 		dbClient = new DBClient();
-		dbClient.clear();
-
-		System.out.println(new File(".").getCanonicalPath());
-
 		classifier = new ImageClassifier("data/syn_set.txt");
+		displayMainMenu();
+	}
 
-		System.out.println(testvideo);
+	private static void displayMainMenu() {
+		System.out.println("What do you want to do?");
+		System.out.println("  [0] Run classification on database");
+		System.out.println("  [1] Execute query based on labels");
+		System.out.println("  [2] Execute query based on labels and dominant colors");
+		System.out.println("  [3] Execute query based on query image");
+		System.out.println("  [4] Execute query based on query image URL");
+		System.out.println("  [C] Clear the image descriptor database");
+		readMenuInput();
+	}
 
+	private static void readMenuInput() {
+		System.out.print("> ");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		char input;
+
+		try {
+			input = reader.readLine().charAt(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+			readMenuInput();
+			return;
+		}
+
+		switch (input) {
+			case '0':
+				runClassification();
+				break;
+
+			case '1':
+				System.out.println("not implemented");
+				break;
+
+			case '2':
+				System.out.println("not implemented");
+				break;
+
+			case '3':
+				System.out.println("not implemented");
+				break;
+
+			case '4':
+				System.out.println("not implemented");
+				break;
+
+			case 'C':
+				dbClient.clear();
+				break;
+
+			default:
+				System.out.println("Unknown option " + input);
+		}
+
+		displayMainMenu();
+	}
+
+	private static void runClassification() {
 		VideoAnalyzer va = new VideoAnalyzer();
 		ArrayList<Mat> frames = va.extractKeyFrames(testvideo, 1, 0.4);
-		saveFrames(frames);
-	}
-	
-	private static void saveFrames(ArrayList<Mat> frames) {
+
 		String imgPath = path + "/Imgs/";
 		for(int i = 0; i<frames.size(); i++) {
 			Mat frame = frames.get(i);
