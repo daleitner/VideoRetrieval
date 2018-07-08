1d57a2a6bed2285eb4ad06b64abd8586d39be079
Jameson Steiner <jameson.steiner@gmail.com>
2018-07-08 18:57:40 +0200

    Add method for submitting the result via a HTTP GET request

--- VideoRetrieval/videoretrieval/src/main/java/videoretrieval/main.java
+++ VideoRetrieval/videoretrieval/src/main/java/videoretrieval/main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -303,4 +304,27 @@ public class main {
 
 		return videoFileNames;
 	}
+
+    private static void submitResult(String videoId, String frameNum) throws Exception {
+        String url = "http://demo2.itec.aau.at:80/vbs/aau/submit?team=%s&video=%s&frame=%s";
+        url = String.format(url, "1", videoId, frameNum);
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("HTTP GET " + url);
+        System.out.println("\tResponse Code: " + responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        System.out.println("\tResponse: " + response.toString());
+    }
 }
